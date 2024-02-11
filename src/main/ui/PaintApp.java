3a26a3832ec40ce70782defb32ab74f634e295f6
Zander Holoboff <zanderholoboff@gmail.com>
2024-02-10 17:44:47 -0800

    Added new class PaintApp

---
+++ src/main/ui/PaintApp.java
@@ -0,0 +1,12 @@
+package ui;
+
+import java.util.Scanner;
+
+// represents a paint application with ui
+public class PaintApp {
+    private Scanner input;
+
+    public PaintApp() {
+        //stub
+    }
+}
