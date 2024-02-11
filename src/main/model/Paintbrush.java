bd2dcab5e036d51c40a1773cdb87102d15fff4e0
Zander Holoboff <zanderholoboff@gmail.com>
2024-02-10 17:25:10 -0800

    Renamed MyModel class to Paintbrush and added fields and constructor

---
+++ src/main/model/Paintbrush.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Paintbrush {
+    private int opacity;
+    private int size;
+    private int red;
+    private int green;
+    private int blue;
+
+    // EFFECTS: constructs a Paintbrush
+    public Paintbrush() {
+        //stub
+    }
+}
