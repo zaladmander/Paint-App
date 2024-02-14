752308e0d66e842ef4a17599daa375a59dfac328
Zander Holoboff <zanderholoboff@gmail.com>
2024-02-14 13:01:34 -0800

    Created class Case

---
+++ src/main/model/Case.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.List;
+
+// represents a case that can hold brushes of any type
+public class Case {
+    private List<Brush> brushes;
+
+    public Case() {
+        //TODO: stub
+    }
+}
