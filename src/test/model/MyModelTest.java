f69ea4d7d502dbdb401c6885caac01a005dcc784
zanderholoboff <zanderholoboff@gmail.com>
2024-02-15 09:26:52 -0800

    Added tests for setRed()

--- src/test/model/MyModelTest.java
+++ src/test/model/MyModelTest.java
@@ -19,4 +19,22 @@ class BrushTest {
         b1.setOpacity(0);
         assertEquals(0, b1.getOpacity());
     }
+
+    @Test
+    void testSetRedOverMax() {
+        b1.setRed(256);
+        assertEquals(0, b1.getRed());
+    }
+
+    @Test
+    void testSetRedInBetweenRange() {
+        b1.setRed(255);
+        assertEquals(255, b1.getRed());
+    }
+
+    @Test
+    void testSetRedUnderMin() {
+        b1.setRed(-1);
+        assertEquals(0, b1.getRed());
+    }
 }
