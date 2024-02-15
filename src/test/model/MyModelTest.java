6f8d1a0a186c4c478978ac381f665ca00f4acfd3
Zander Holoboff <zanderholoboff@gmail.com>
2024-02-15 08:51:42 -0800

    Added imports, fields, setup, tests

--- src/test/model/MyModelTest.java
+++ src/test/model/MyModelTest.java
@@ -1,5 +1,22 @@
 package model;
 
-class BrushTest {
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
+class BrushTest {
+    Brush b1;
+    Brush b2;
+
+    @BeforeEach
+    void setup() {
+        b1 = new Brush(1, 10);
+        b2 = new Brush(1, 50);
+    }
+
+    @Test
+    void testSetOpacityToOkayNumber() {
+        b1.setOpacity(0);
+        assertEquals(0, b1.getOpacity());
+    }
 }
