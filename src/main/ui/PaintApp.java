d4cddbf7df19954b27bc66f6276fbac6f909f631
Zander Holoboff <zanderholoboff@gmail.com>
2024-02-16 09:13:41 -0800

    Added imports, Case field, methods

--- src/main/ui/PaintApp.java
+++ src/main/ui/PaintApp.java
@@ -1,12 +1,44 @@
 package ui;
 
+import model.Brush;
+import model.Case;
+
 import java.util.Scanner;
 
 // represents a paint application with ui
 public class PaintApp {
+    private Case pencilCase;
     private Scanner input;
 
     public PaintApp() {
-        //stub
+        runPaintApp();
+    }
+
+    private void runPaintApp() {
+
+    }
+
+    private void displayMainMenu() {
+        System.out.println("Press 'c' to look at your brushes");
+        System.out.println("Press 'q' to quit");
+    }
+
+    private void displayListOfBrushes() {
+        for (Brush b : pencilCase.getBrushes()) {
+            System.out.println();
+        }
+    }
+
+    private void displayBrush() {
+
+        System.out.println("Press 'e' to edit your brush");
+    }
+
+    private void displayEditMenu() {
+
+    }
+
+    private void initialize() {
+
     }
 }
