a1c46231dfa7ba8b2925a762bf80b96d79dc971b
Zander Holoboff <zanderholoboff@gmail.com>
2024-02-19 00:07:14 -0800

    rearranged tests to be in order of methods

--- src/test/model/BrushTest.java
+++ src/test/model/BrushTest.java
@@ -22,6 +22,48 @@ class BrushTest {
     }
 
     @Test
+    void testSetOpacityOverMax() {
+        b1.setOpacity(1.5);
+        assertEquals(1, b1.getOpacity());
+    }
+
+    @Test
+    void testSetOpacityMaxBound() {
+        b1.setOpacity(1);
+        assertEquals(1, b1.getOpacity());
+    }
+
+    @Test
+    void testSetOpacityUnderMin() {
+        b1.setOpacity(-0.3);
+        assertEquals(0, b1.getOpacity());
+    }
+
+    @Test
+    void testSetOpacityMinBound() {
+        b1.setOpacity(0);
+        assertEquals(0, b1.getOpacity());
+    }
+
+    @Test
+    void testSetOpacityDouble() {
+        b1.setOpacity(0.35897);
+        assertEquals(0.35897, b1.getOpacity());
+    }
+
+    @Test
+    void testSetSizeLessThanBounds() {
+        b1.setSize(0);
+        assertEquals(1, b1.getSize());
+    }
+
+    @Test
+    void testSetSize() {
+        b1.setSize(256);
+        assertEquals(256, b1.getSize());
+    }
+    
+    @Test
     void testSetRedOverMax() {
         b1.setRed(256);
         assertEquals(255, b1.getRed());
@@ -73,48 +115,6 @@ class BrushTest {
     void testSetBlueUnderMin() {
         b1.setBlue(-1);
         assertEquals(0, b1.getBlue());
-    }
-
-    @Test
-    void testSetOpacityOverMax() {
-        b1.setOpacity(1.5);
-        assertEquals(1, b1.getOpacity());
-    }
-
-    @Test
-    void testSetOpacityMaxBound() {
-        b1.setOpacity(1);
-        assertEquals(1, b1.getOpacity());
-    }
-
-    @Test
-    void testSetOpacityUnderMin() {
-        b1.setOpacity(-0.3);
-        assertEquals(0, b1.getOpacity());
-    }
-
-    @Test
-    void testSetOpacityMinBound() {
-        b1.setOpacity(0);
-        assertEquals(0, b1.getOpacity());
-    }
-
-    @Test
-    void testSetOpacityDouble() {
-        b1.setOpacity(0.35897);
-        assertEquals(0.35897, b1.getOpacity());
-    }
-
-    @Test
-    void testSetSizeLessThanBounds() {
-        b1.setSize(0);
-        assertEquals(1, b1.getSize());
-    }
-
-    @Test
-    void testSetSize() {
-        b1.setSize(256);
-        assertEquals(256, b1.getSize());
     }
 
     @Test
