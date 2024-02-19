0242250318448c7755a4469400e07c10d40ddca8
Zander Holoboff <zanderholoboff@gmail.com>
2024-02-18 20:17:19 -0800

    Added more methods for more menus

--- src/main/ui/PaintApp.java
+++ src/main/ui/PaintApp.java
@@ -3,12 +3,14 @@ package ui;
 import model.Brush;
 import model.Case;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 // represents a paint application with ui
 public class PaintApp {
-    private Case pencilCase;
     private Scanner input;
+    private List<Case> cases;
 
     public PaintApp() {
         runPaintApp();
@@ -16,10 +18,14 @@ public class PaintApp {
 
     private void runPaintApp() {
         boolean run = true;
-        String command = null;
-
         initialize();
 
+        runMainMenu(run);
+        System.out.println("Quitting...");
+    }
+
+    private void runMainMenu(boolean run) {
+        String command = null;
         while (run) {
             displayMainMenu();
             command = input.next();
@@ -27,26 +33,73 @@ public class PaintApp {
             if (command.equals("q")) {
                 run = false;
             } else {
-                processCommand(command);
+                processMainCommand(command);
             }
         }
-        System.out.println("Quitting...");
     }
 
-    private void processCommand(String command) {
-        if (command.equals("c")) {
-            displayListOfBrushes();
+    private void runCasesMenu(boolean run) {
+        String command = null;
+        while (run) {
+            displayCasesMenu();
+            command = input.next();
+
+            if (command.equals("q")) {
+                run = false;
+            } else {
+                processCasesCommand(command);
+            }
+        }
+    }
+
+    private void processMainCommand(String command) {
+        if (command.equals("d")) {
+            draw();
+        } else if (command.equals("c")) {
+            runCasesMenu(true);
         } else {
             System.out.println("invalid input...");
         }
     }
 
+    private void processCasesCommand(String command) {
+        if (command.equals("m")) {
+            System.out.println("Name your case");
+            String name = input.next();
+            cases.add(new Case(name));
+        } else if (command.equals("o")) {
+            System.out.println(cases);
+        } else {
+            System.out.println("invalid input...");
+        }
+    }
+
+    private void draw() {
+        System.out.println("This functionality does not work yet!");
+    }
+
     private void displayMainMenu() {
-        System.out.println("Press 'c' to look at your brushes");
+        System.out.println("Press 'd' to draw on a new canvas");
+        System.out.println("Press 'c' to look at brushes");
         System.out.println("Press 'q' to quit");
     }
 
-    private void displayListOfBrushes() {
+    private void displayCasesMenu() {
+        String command = null;
+        System.out.println("Your cases:");
+        if (cases.isEmpty()) {
+            System.out.println("You have no cases.");
+        } else {
+            for (Case c : cases) {
+                System.out.println(c.getName());
+            }
+        }
+        System.out.println("Press 'm' to make a case");
+        System.out.println("Press 'o' to look into a case");
+        System.out.println("Press 'q' to go back");
+    }
+
+    private void displayListOfBrushes(Case pencilCase) {
         if (pencilCase.getBrushes().isEmpty()) {
             System.out.println("No brushes :(");
         } else {
@@ -56,17 +109,21 @@ public class PaintApp {
         }
     }
 
-    private void displayBrush() {
+    private void displayBrushMenu() {
 
         System.out.println("Press 'e' to edit your brush");
     }
 
     private void displayEditMenu() {
-
+        System.out.println("(1) - Color");
+        System.out.println("(2) - Opacity");
+        System.out.println("(3) - Name");
+        System.out.println("(4) - Size");
+        System.out.println("or press 'q' to go back");
     }
 
     private void initialize() {
-        pencilCase = new Case("default case");
         input = new Scanner(System.in);
+        cases = new ArrayList<>();
     }
 }
