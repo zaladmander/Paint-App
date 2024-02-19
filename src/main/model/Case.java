ba0c58d7b8d7717187a02ad8afff3f1b23895efd
Zander Holoboff <zanderholoboff@gmail.com>
2024-02-19 01:27:00 -0800

    added documentation

--- src/main/model/Case.java
+++ src/main/model/Case.java
@@ -10,30 +10,44 @@ public class Case {
     private List<Brush> brushes;
     private String name;
 
+    // EFFECTS: constructs a case with empty list brushes and a name
     public Case(String name) {
         brushes = new ArrayList<>();
         this.name = name;
     }
 
+    // MODIFIES: this
+    // EFFECTS: adds given brush to the brushes list
     public void addBrush(Brush brush) {
         this.brushes.add(brush);
     }
 
     // REQUIRES: index in brushes list range and brushes must not be empty
+    // MODIFIES: this
+    // EFFECTS: removes brush at index from brushes
     public void removeBrush(int index) {
         this.brushes.remove(index);
     }
 
+    // setters
+
+    // MODIFIES: this
+    // EFFECTS: sets name to given name
     public void setName(String name) {
         this.name = name;
     }
 
+    // getters
+
     // REQUIRES: index is in brushes list range
+    // EFFECTS: returns brush at index from brushes list
     public Brush getBrush(int index) {
         return this.brushes.get(index);
     }
 
-    // REQUIRES: // TODO i forgor
+    // EFFECTS: with given string, returns brush with same name from brushes
+    //          if brushes is empty or there is no brush with given name,
+    //          return null
     public Brush getBrushWithName(String name) {
         for (Brush b: brushes) {
             if (b.getName().equals(name)) {
