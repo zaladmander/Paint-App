831e39ab4277a5573042805f5db7b1a43dcc3dcf
zanderholoboff <zanderholoboff@gmail.com>
2024-03-07 09:18:51 -0800

    added toJson() interface method

--- src/main/persistence/Writable.java
+++ src/main/persistence/Writable.java
@@ -1,4 +1,11 @@
 package persistence;
 
+import org.json.JSONObject;
+
 public interface Writable {
+    //Referenced from JsonSerializationDemo
+    //https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
+
+    // EFFECTS: returns this as JSON object
+    JSONObject toJson();
 }
