6cdef2233d9974b8f37fa88627e30f3643226069
zanderholoboff <zanderholoboff@gmail.com>
2024-03-08 11:17:38 -0800

    updated tests

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -1,5 +1,6 @@
 package persistence;
 
+import model.Brush;
 import model.BrushesRoom;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +9,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class JsonReaderTest {
+public class JsonReaderTest extends JsonTest{
 
     @Test
     void testSourceNotFound() {
@@ -27,6 +28,40 @@ public class JsonReaderTest {
         try {
             BrushesRoom br = jsonReader.readBrushesRoom();
             assertEquals(0, br.getCases().size());
+        } catch (IOException e) {
+            fail("Didn't expect an exception");
+        }
+    }
+
+    @Test
+    void testReaderCasesNoBrushes() {
+        JsonReader jsonReader = new JsonReader("./data/testReaderCasesNoBrushes.json");
+        try {
+            BrushesRoom br = jsonReader.readBrushesRoom();
+            assertEquals(2, br.getCases().size());
+            assertEquals("sponges", br.getCases().get(0).getName());
+            assertTrue(br.getCases().get(0).getBrushes().isEmpty());
+            assertEquals("felts", br.getCases().get(1).getName());
+            assertTrue(br.getCases().get(1).getBrushes().isEmpty());
+        } catch (IOException e) {
+            fail("Didn't expect an exception");
+        }
+    }
+
+    @Test
+    void testReaderCasesAllBrushesRoom() {
+        JsonReader jsonReader = new JsonReader("./data/testReaderAllBrushesRoom.json");
+        try {
+            BrushesRoom br = jsonReader.readBrushesRoom();
+            Brush b1 = br.getCases().get(0).getBrushes().get(0);
+            Brush b2 = br.getCases().get(1).getBrushes().get(0);
+            assertEquals(2, br.getCases().size());
+            assertEquals("realistic", br.getCases().get(0).getName());
+            assertFalse(br.getCases().get(0).getBrushes().isEmpty());
+            checkBrushValues(b1, 50, "tree", 100, 100, 0, 1);
+            assertEquals("stamps", br.getCases().get(1).getName());
+            assertFalse(br.getCases().get(1).getBrushes().isEmpty());
+            checkBrushValues(b2, 50, "face", 50, 50, 50, 1);
         } catch (IOException e) {
             fail("Didn't expect an exception");
         }
--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -1,4 +1,18 @@
 package persistence;
 
+import model.Brush;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class JsonTest {
+    public void checkBrushValues(Brush brush, int size,
+                                 String name, int red, int green,
+                                 int blue, double opacity) {
+        assertEquals(name, brush.getName());
+        assertEquals(size, brush.getSize());
+        assertEquals(red, brush.getRed());
+        assertEquals(green, brush.getGreen());
+        assertEquals(blue, brush.getBlue());
+        assertEquals(opacity, brush.getOpacity());
+    }
 }
