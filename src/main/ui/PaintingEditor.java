1a6ee589e66877b82b6613ac12e0a08ae5318dce
zanderholoboff <zanderholoboff@gmail.com>
2024-03-26 00:08:02 -0700

    playing around with JFrames and JLabels

--- src/main/ui/PaintingEditor.java
+++ src/main/ui/PaintingEditor.java
@@ -1,5 +1,7 @@
 package ui;
 
+import ui.tools.ColorSelecterTool;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -14,9 +16,35 @@ public class PaintingEditor extends JFrame {
 
     private void initializeWindow() {
         setLayout(new BorderLayout());
-        setMinimumSize(new Dimension(WIDTH, HEIGHT));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setMinimumSize(new Dimension(WIDTH - 200, HEIGHT - 150));
+        setSize(WIDTH, HEIGHT);
+        getContentPane().setBackground(new Color(220, 220, 200));
+
+        initializeTitleText(new JLabel());
+        initializeToolBar();
+
         setLocationRelativeTo(null);
         setVisible(true);
+    }
+
+    private void initializeTitleText(JLabel titleText) {
+        titleText.setText("Hello");
+        titleText.setFont(new Font("Times New Roman", Font.PLAIN, 100));
+        titleText.setVerticalAlignment(JLabel.CENTER);
+        titleText.setHorizontalAlignment(JLabel.CENTER);
+
+        titleText.setBackground(new Color(200,30,120));
+        titleText.setOpaque(true);
+        this.add(titleText);
+    }
+
+    private void initializeToolBar() {
+        JPanel toolBar = new JPanel();
+        toolBar.setLayout(new GridLayout(3,0));
+        toolBar.setSize(new Dimension(0, 0));
+        add(toolBar, BorderLayout.NORTH);
+
+        //ColorSelecterTool rectTool = new ColorSelecterTool(this, toolBar);
     }
 }
